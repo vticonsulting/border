a264f9cbbc57e663d9b34499b700ab3e5e3cb8d2
Victor Tolbert <victor.tolbert@gmail.com>
2021-09-17 17:22:14 -0400

    ✨ init

---
+++ index.js
@@ -0,0 +1,18 @@
+const border = (options) => {
+  let elements = document.querySelectorAll(".border");
+
+  if (options.borderType === "thick") {
+    options.borderType = "3px";
+  } else {
+    options.borderType = "1px";
+  }
+  if (!options.borderColor) {
+    options.borderColor = "black";
+  }
+
+  elements.forEach((element) => {
+    element.style.border = `${options.borderType} solid ${options.borderColor}`;
+  });
+};
+
+module.exports.border = border;
